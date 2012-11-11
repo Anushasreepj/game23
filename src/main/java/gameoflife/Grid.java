634aa7595700f82bd027c9ec1010f263073dd332
Danny Smith <dansmith1979@gmail.com>
2012-11-11 22:44:06 +0000

    Test equality of Grid

---
+++ src/main/java/gameoflife/Grid.java
@@ -0,0 +1,44 @@
+package gameoflife;
+
+import java.awt.*;
+import java.util.Collections;
+import java.util.Set;
+
+public class Grid {
+
+    private final int width;
+    private final int height;
+    private final Set<Point> activeCells;
+
+    public Grid(int width, int height) {
+        this(width, height, Collections.<Point>emptySet());
+    }
+
+    public Grid(int width, int height, Set<Point> activeCells) {
+        this.width = width;
+        this.height = height;
+        this.activeCells = activeCells;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Grid grid = (Grid) o;
+
+        if (height != grid.height) return false;
+        if (width != grid.width) return false;
+        if (activeCells != null ? !activeCells.equals(grid.activeCells) : grid.activeCells != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = width;
+        result = 31 * result + height;
+        result = 31 * result + (activeCells != null ? activeCells.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/test/java/gameoflife/GameOfLifeTest.java
@@ -0,0 +1,37 @@
+package gameoflife;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.awt.*;
+import java.util.HashSet;
+
+import static org.hamcrest.CoreMatchers.*;
+
+public class GameOfLifeTest {
+
+    @Test
+    public void Grid_equality_is_correct() {
+
+        Grid grid1 = new Grid(4, 3);
+        Grid grid2 = new Grid(4, 3);
+        Grid grid3 = new Grid(5, 2);
+        Grid grid4 = new Grid(4, 3, new HashSet<Point>() {{
+            add(new Point(1,2));
+        }});
+        Grid grid5 = new Grid(4, 3, new HashSet<Point>() {{
+            add(new Point(1,2));
+        }});
+        Grid grid6 = new Grid(4, 3, new HashSet<Point>() {{
+            add(new Point(2,3));
+            add(new Point(3,4));
+        }});
+
+        Assert.assertThat(grid1, equalTo(grid2));
+        Assert.assertThat(grid1, not(equalTo(grid3)));
+        Assert.assertThat(grid4, equalTo(grid5));
+        Assert.assertThat(grid5, not(equalTo(grid6)));
+        Assert.assertThat(grid1, not(equalTo(grid4)));
+
+    }
+}
